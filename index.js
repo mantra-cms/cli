6f56e035e6fff9aa318c54356300a21cc82c5e89
Denyncrawford <kiquerangelb@gmail.com>
2020-07-12 14:54:51 -0300

    mantra init

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S deno run --unstable
+
+/* const initiator = async () => {
+  return await prompt();
+} */
+
+// Main imports and Declarations
+
+ 
+import { styles } from "https://deno.land/x/ansi_styles/mod.ts";
+import { text } from 'https://raw.githack.com/denyncrawford/deno-figlet/master/mod.js';
+
+const args = Deno.args
+const dir = Deno.cwd();
+
+import { Command } from 'https://deno.land/x/cliffy/command.ts';  
+
+// ClI Modules import
+
+import { create } from './lib/deps/deps.js';
+
+// CLI Command Executor:
+
+console.log(`${styles.green.open}${await text("Mantra CLI","standard")}${styles.green.close}`);
+
+await new Command()
+  .name("mantra")
+  .version('0.1.0')
+  .allowEmpty(false)
+  .command('create [name]')
+  .description("Creates a new mantra project.")
+  .action(async (opt , name) => {
+    let setup = await create.prompt(dir, name)
+    create.init(dir, setup)    
+  })
+  .parse(args);
